850bcb661cea26e7f6011300f6ef4f75d1cce0b8
orianemgn <mgnoriane@gmail.com>
2022-12-01 18:51:52 +0100

    flip working half way

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import styles from "./card.module.css";
-//import { useState } from "react";
+import { useState } from "react";
 
 function Card({
   img,
@@ -12,7 +12,9 @@ function Card({
   setPrevIndex,
   solvedMemories,
 }) {
+  const [flip, setFlip] = useState(false);
   function handleClick() {
+    setFlip(!flip);
     if (prevIndex !== index) {
       if (!solvedMemories.includes(index)) {
         setClickedCards((prev) => [...prev, id]);
@@ -25,9 +27,17 @@ function Card({
   }
   return (
     <div className={styles.card} onClick={handleClick}>
-      <div className={styles.flipCardInner}>
-        <div className={styles.flipCardFront}>Hello</div>
-        <div className={styles.flipCardBack}>
+      <div
+        className={
+          flip
+            ? `${styles.innerCard} ${styles.isFlipped}`
+            : `${styles.innerCard}`
+        }
+      >
+        <div className={`${styles.cardFace} ${styles.cardFaceFront}`}>
+          Front
+        </div>
+        <div className={`${styles.cardFace} ${styles.cardFaceBack}`}>
           <img className={styles.cardImg} src={img} alt={name} />
         </div>
       </div>
@@ -35,3 +45,12 @@ function Card({
   );
 }
 export default Card;
+
+//<div className={styles.card} onClick={handleClick}>
+//  <div className={styles.flipCardInner}>
+//    <div className={styles.flipCardFront}>Hello</div>
+//    <div className={styles.flipCardBack}>
+//      <img className={styles.cardImg} src={img} alt={name} />
+//    </div>
+//  </div>
+//</div>;
