d8ec13879d6f1b247fd8e6260f4eb0e8966e995b
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-12-01 19:43:50 +0100

    cards content created

---
+++ src/components/memoryBoard/CardsData.jsx
@@ -0,0 +1,60 @@
+import picture1 from "../../assets/fuji.jpg";
+import picture2 from "../../assets/golden_bridge.jpg";
+import picture3 from "../../assets/lempuyang.jpg";
+import picture4 from "../../assets/sphinx.jpg";
+import picture5 from "../../assets/peles_castle.jpg";
+import picture6 from "../../assets/triumphal_arch.jpg";
+import picture7 from "../../assets/tv_tower.jpg";
+import picture8 from "../../assets/liberty.jpg";
+
+const CardsData = [
+  {
+    id: 1,
+    img: picture1,
+    name: "fuji",
+    info: "Mount Fuji (富士山, Fujisan) is with 3776 meters Japan's highest mountain. It is not surprising that the nearly perfectly shaped volcano has been worshiped as a sacred mountain and experienced big popularity among artists and common people throughout the centuries. Mount Fuji is an active volcano, which most recently erupted in 1707. It stands on the border between Yamanashi and Shizuoka prefectures and can be seen from Tokyo and Yokohama on clear days.",
+  },
+  {
+    id: 2,
+    img: picture2,
+    name: "golden_bridge",
+    info: "The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide (1.6 km) strait connecting San Francisco Bay and the Pacific Ocean. Acclaimed as one of the world's most beautiful bridges, there are many different elements to the Golden Gate Bridge that make it unique. With its tremendous towers, sweeping cables, and great span, the Bridge is a sensory beauty and engineering wonder featuring color, sound and light.",
+  },
+  {
+    id: 3,
+    img: picture3,
+    name: "lempuyang",
+    info: "Pura Penataran Agung Lempuyang is a Balinese Hindu temple or pura located in the slope of Mount Lempuyang in Karangasem, Bali. Pura Penataran Agung Lempuyang is considered as part of a complex of pura surrounding Mount Lempuyang, one of the highly regarded temples of Bali.",
+  },
+  {
+    id: 4,
+    img: picture4,
+    name: "sphinx",
+    info: "The Great Sphinx at Giza, near Cairo, is probably the most famous sculpture in the world. With a lion's body and a human head, it represents Ra-Horakhty, a form of the powerful sun god, and is the incarnation of royal power and the protector of the temple doors.",
+  },
+  {
+    id: 5,
+    img: picture5,
+    name: "Peleș Castle is a Neo-Renaissance castle in the Carpathian Mountains, near Sinaia, in Prahova County, Romania, on an existing medieval route linking Transylvania and Wallachia, built between 1873 and 1914. Its inauguration was held in 1883. It was constructed for King Carol I.",
+  },
+  {
+    id: 6,
+    img: picture6,
+    name: "triumphal_arch",
+    info: "A triumphal arch is a free-standing monumental structure in the shape of an archway with one or more arched passageways, often designed to span a road.",
+  },
+  {
+    id: 7,
+    img: picture7,
+    name: "tv_tower",
+    info: "The Berliner Fernsehturm or Fernsehturm Berlin is a television tower in central Berlin, Germany.Located in the Marien quarter (Marienviertel), close to Alexanderplatz in the locality and district of Mitte, the tower was constructed between 1965 and 1969 by the government of the German Democratic Republic (East Germany). It was intended to be both a symbol of Communist power and of the city. It remains a landmark today, visible throughout the central and some suburban districts of Berlin.[1] With its height of 368 metres (including antenna) it is the tallest structure in Germany, and the third-tallest structure in the European Union.",
+  },
+  {
+    id: 8,
+    img: picture8,
+    name: "liberty",
+    info: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City, in the United States. The copper statue, a gift from the people of France, was designed by French sculptor Frédéric Auguste Bartholdi and its metal framework was built by Gustave Eiffel.",
+  },
+];
+
+export default CardsData;
