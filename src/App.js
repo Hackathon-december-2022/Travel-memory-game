0176bcc900deaa571bc0574198f4a387259b4ad8
MarianneH <marianne.helbig@googlemail.com>
2022-12-01 14:10:14 +0100

    added about us page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Rules from "./pages/rules/Rules";
 import Home from "./pages/home/Home";
+import AboutUs from "./pages/aboutUs/AboutUs";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route element={<Home />} exact path="/" />
           <Route element={<Rules />} path="/rules" />
+          <Route element={<AboutUs />} path="/about-us" />
         </Routes>
       </Router>
     </div>
