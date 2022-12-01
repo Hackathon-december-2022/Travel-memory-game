bd3c1a2f880fc7c0088ae1afeabf0d106fd00ca5
RobertKaniuth <robert.kaniuth@gmx.de>
2022-12-01 18:19:31 +0100

    header & footer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Rules from "./pages/rules/Rules";
 import Home from "./pages/home/Home";
 import AboutUs from "./pages/aboutUs/AboutUs";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route element={<Home />} exact path="/" />
           <Route element={<Rules />} path="/rules" />
           <Route element={<AboutUs />} path="/about-us" />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
