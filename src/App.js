b9222f9e465607ace069312e6f65dd773d31861e
MarianneH <marianne.helbig@googlemail.com>
2022-12-01 14:08:42 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Rules from "./pages/rules/Rules";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div>
-      <h1>Travel Memory game</h1>
+      <Router>
+        <Routes>
+          <Route element={<Home />} exact path="/" />
+          <Route element={<Rules />} path="/rules" />
+        </Routes>
+      </Router>
     </div>
   );
 }
